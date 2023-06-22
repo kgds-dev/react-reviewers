81ad8f63072a827b71bb88fafecf0c242f8d1503
Kimbo Graham Sanz <kimbograham.sanz@gmail.com>
2023-06-22 17:14:56 +0800

    added vercel baseUrl for the api service call

--- src/api-service/apiService.js
+++ src/api-service/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://express-js-reviewers.vercel.app/',
 });
 
 export default apiService;
